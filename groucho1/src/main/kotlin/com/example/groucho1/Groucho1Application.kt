1cefc2bf4bf9b488cd174cc0f789885e2e199461
Mario Gray <mario.gray@gmail.com>
2020-07-15 23:41:10 -0700

    init

---
+++ groucho1/src/main/kotlin/com/example/groucho1/Groucho1Application.kt
@@ -0,0 +1,11 @@
+package com.example.groucho1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Groucho1Application
+
+fun main(args: Array<String>) {
+	runApplication<Groucho1Application>(*args)
+}
---
+++ groucho1/src/test/kotlin/com/example/groucho1/Groucho1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.groucho1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Groucho1ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
